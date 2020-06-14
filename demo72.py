3db12a6e1eb972ee6e99ccd0fc433c1c511c5ae9
liv <liv.hsieh@cvilux.com>
2020-06-14 16:59:47 +0800

    day 5 update afternoon 2-2

--- demo72.py
+++ demo72.py
@@ -14,3 +14,14 @@ model.add(Flatten())
 model.add(Dense(64, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 print(model.summary())
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+train_images = train_images.reshape((60000, 28, 28, 1))
+train_images = train_images.astype('float32') / 255
+test_imagets = test_images.reshape((10000, 28, 28, 1))
+test_images = test_images.astype('float32') / 255
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=5, batch_size=100)
